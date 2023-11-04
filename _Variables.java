0e8e1c7cf664f3fa3e6f8843bf307c29a7bf1f11
yahlikleinfeld <yahli.kleinfeld@gmail.com>
2023-11-04 14:23:03 +0200

    init java

---
+++ _Variables.java
@@ -0,0 +1,22 @@
+public class _Variables {
+    public static void main(String[] args) {
+        System.out.println("Variables");
+        int age = 30;
+        System.out.println(age); // 30
+        age = 35;
+        System.out.println(age); // 35
+        int age2 = age;
+        System.out.println(age2); // 35
+
+        byte floor = 3;
+        int number = 12;
+        long postcode = 2092030923092L; // L
+        double entrance = 3.5;
+        float size = 149.99F; // F
+        char entranceLetter = 'B';
+        boolean fun = true; // false
+
+
+    }
+    
+}
---
+++ com/codewithyahli/_FirstJavaProject.java
@@ -0,0 +1,9 @@
+package com.codewithyahli;
+
+public class _FirstJavaProject {
+    public static void main(String[] args) {
+        System.out.println("hi");
+
+    }
+    
+}
